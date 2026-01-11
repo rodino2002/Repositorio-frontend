faa0da43742678ec03c0c4cb99d388aa7287c506
feliciano <rodinofeliciano2002@gmail.com>
2026-01-11 19:19:12 +0100

    Projecto Inicializado com login

---
+++ src/components/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+
+export default function Dashboard() {
+
+  return (
+    <>
+      <div className='flex justify-center w-full bg-amber-300 p-4 rounded-2xl'>
+        Página de Dashboard
+      </div>
+        
+    </>
+  )
+}
+
---
+++ src/components/pages/login.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center w-full bg-amber-300 p-4 rounded-2xl'>
+        
+      </div>
+        
+    </>
+  )
+}
+
+export default App
